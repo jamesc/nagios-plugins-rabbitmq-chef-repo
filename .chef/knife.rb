861f1966bca204f00122cc3922b02bdd00e546ab
jamesc <james@opscode.com>
2013-09-18 22:00:38 -0700

    correct location of validator

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -4,7 +4,7 @@ log_location             STDOUT
 node_name                "jamesc"
 client_key               "#{ENV['HOME']}/.chef/#{ENV['USER']}.pem"
 validation_client_name   "rabbitmq_test-validator"
-validation_key           "#{ENV['HOME']}/.chef/rabbitmq_test-validator.pem"
+validation_key           "#{ENV['HOME']}/.chef/validators/rabbitmq_test-validator.pem"
 chef_server_url          "https://api.opscode.com/organizations/rabbitmq_test"
 cache_type               'BasicFile'
 cache_options( :path => "#{ENV['HOME']}/.chef/checksums" )
