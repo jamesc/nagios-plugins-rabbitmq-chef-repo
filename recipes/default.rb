88f5ef45992f60029db493a8b622b20382914063
jamesc <james@opscode.com>
2013-09-18 22:01:36 -0700

    symlink in scripts to nagios plugin dir

--- recipes/default.rb
+++ recipes/default.rb
@@ -5,6 +5,7 @@
 # Copyright 2012, Opscode, Inc
 #
 
+include_recipe "rabbitmq"
 include_recipe "rabbitmq::mgmt_console"
 
 %w{libnagios-plugin-perl libjson-perl}.each do |pkg|
@@ -20,3 +21,12 @@ git "/opt/nagios-plugins-rabbitmq" do
   action :sync
 end
 
+scripts = %w{aliveness objects overview queue server watermark}
+
+scripts.each do |script|
+  link "/usr/lib/nagios/plugins/check_rabbitmq_#{script}" do
+    action :create
+    to "/opt/nagios-plugins-rabbitmq/scripts/check_rabbitmq_#{script}"
+  end
+
+end
